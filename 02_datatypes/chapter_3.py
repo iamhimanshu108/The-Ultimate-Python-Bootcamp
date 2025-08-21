47d697dc5e7e582484219d025439e5c41ad8cdc5
Himanshu Kumar (Yadav) <110648951+iamhimanshu108@users.noreply.github.com>
2025-08-21 09:53:46 +0000

    datatypes

--- 02_datatypes/chapter_3.py
+++ 02_datatypes/chapter_3.py
@@ -7,3 +7,19 @@ print(f"Total Gram {total_gram}")
 
 remain_tea = black_tea_gram*giner_gram
 print(f"Total Remaining {remain_tea}")
+
+milk_litre =7
+serving =4
+milk_per_serving = milk_litre/serving
+print(f"Milk per serving {milk_per_serving}")
+
+total_tea_bags = 7
+pots = 4
+bagsperpots=total_tea_bags//pots
+print(f"Bags per pots {bagsperpots}")
+
+
+total_cadamon_pods = 10
+pods_per_cup =3
+left = total_cadamon_pods %pods_per_cup
+print(f"leftover cups  {left}")
