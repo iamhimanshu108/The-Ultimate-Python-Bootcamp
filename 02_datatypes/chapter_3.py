038f171f7f3e4faec0e8285ae1a28d4316da208f
Himanshu Kumar (Yadav) <110648951+iamhimanshu108@users.noreply.github.com>
2025-08-21 06:10:44 +0000

    Integer

---
+++ 02_datatypes/chapter_3.py
@@ -0,0 +1,9 @@
+# Integer
+black_tea_gram =14
+giner_gram =3
+
+total_gram = black_tea_gram+ giner_gram
+print(f"Total Gram {total_gram}")
+
+remain_tea = black_tea_gram*giner_gram
+print(f"Total Remaining {remain_tea}")
