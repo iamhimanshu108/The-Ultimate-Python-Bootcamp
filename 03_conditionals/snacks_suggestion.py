a0a5aa20a71187b3ebd1f22faf1e952a71716407
Himanshu Kumar (Yadav) <110648951+iamhimanshu108@users.noreply.github.com>
2025-08-25 09:52:23 +0000

    conditionals

---
+++ 03_conditionals/snacks_suggestion.py
@@ -0,0 +1,6 @@
+snack = input ("Enter Your Preferred snack: ").lower()
+
+
+if snack =="cookies" or snack =="samosa":
+    print("Great choice! Enjoy your snack.")
+else: print("Sorry, we  only serve cookies and samosas.")
