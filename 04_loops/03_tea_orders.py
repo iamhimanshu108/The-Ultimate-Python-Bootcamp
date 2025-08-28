a8a126d10c4dc76fca528591da980340de380e7b
Himanshu Kumar (Yadav) <110648951+iamhimanshu108@users.noreply.github.com>
2025-08-28 05:52:45 +0000

    enumarates and loops

---
+++ 04_loops/03_tea_orders.py
@@ -0,0 +1,4 @@
+orders = ["hitesh", "Himanshu", "Manash", "Pankaj"]
+
+for name in orders:
+    print(f"Order is Raedy for : {name}")
---
+++ 04_loops/04_tea_menu.py
@@ -0,0 +1,7 @@
+menu = ["Green" ,"Ginger", "Lemon","Mint"]
+
+for m in menu:
+    print(f"Menu Item is {m}")
+
+for idx, item in enumerate(menu, start=1):
+    print(f"{idx} : {item} chai")
