e8ceaad84a861407571e8750249b8c3df9c5caf4
Himanshu Kumar (Yadav) <110648951+iamhimanshu108@users.noreply.github.com>
2025-10-11 13:23:48 +0000

    First

--- PYTHON/Varaibles and Data Types/first.py
+++ PYTHON/Varaibles and Data Types/first.py
@@ -1,3 +1,20 @@
-print("Hello, World!")
-print("My name is Himanshu")
-print("I am learning Python programming.")
+# print("Hello, World!")
+# print("My name is Himanshu")
+# print("I am learning Python programming.")
+# print(23)
+# print(23 + 45 - 67 * 89 / 90)
+
+
+# name ="Himanshu"
+# age= 25
+# price=23.7
+
+# print(type(name))
+# print(type(age))
+# print(type(price))
+
+# age = 23
+# old = False
+# a= None
+# print(type(old))
+# print(type(a))
